86844dd8fce90387950240d83663fde9dd06fb49
Com-Pensa <65026007+Com-Pensa@users.noreply.github.com>
2020-05-15 20:53:39 -0300

    Delete source.js

--- src/Border_Raidius/source.js
+++
@@ -1,9 +0,0 @@
-var radius_camp = window.document.querySelector("input#input_radius")
-var main_square = window.document.getElementById("previewer")
-main_square.style.borderRadius = "100px"
-
-function preview()
-{   
-    var radius = Number.parseInt(radius_camp.value)
-    main_square.style.borderRadius = `${radius}px`
-}
